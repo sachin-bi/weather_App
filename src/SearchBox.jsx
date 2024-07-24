73762c7ef379d9c8e50c08b7dc2f6aeee9736963
Sachinandan <sachinandanp5@gmail.com>
2024-07-24 12:25:01 +0530

    searchBox ready

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -4,11 +4,26 @@ import "./SearchBox.css";
 import { useState } from "react";
 
 export default function SearchBox() {
-
-    let API_URL = "https://api.openweathermap.org/data/2.5/weather";
-    let API_KEY = "12fc9d90529bf75cb0ecd6c9e3a79e03";
-
   let [city, setCity] = useState("");
+
+  let API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  let API_KEY = "12fc9d90529bf75cb0ecd6c9e3a79e03";
+  let getWeatherInfo = async () => {
+    let response = await fetch(
+      `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    let jsonResponse = await response.json();
+    // console.log(jsonResponse);
+    let result = {
+      temp: jsonResponse.main.temp,
+      tempMin: jsonResponse.main.temp_min,
+      tempMin: jsonResponse.main.temp_max,
+      humidity: jsonResponse.main.humidity,
+      feelsLike: jsonResponse.main.feels_like,
+      weather: jsonResponse.weather[0].description,
+    };
+    console.log(result);
+  };
 
   let handleChange = (evt) => {
     setCity(evt.target.value);
@@ -17,12 +32,13 @@ export default function SearchBox() {
   let handleSubmit = (evt) => {
     evt.preventDefault();
     console.log(city);
+    getWeatherInfo();
     setCity("");
   };
 
   return (
     <div className="SearchBox">
-      <h2>search form box</h2>
+      <h2>Get Weather Details</h2>
       <form onSubmit={handleSubmit}>
         <TextField
           id="city"
